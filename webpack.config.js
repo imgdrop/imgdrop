baa6606fa59889340ac400b7d9118eb82c42abf0
matanui159 <josh@minteronline.com>
2020-04-19 14:24:35 +1000

    Improvements to monitoring and serving

--- webpack.config.js
+++ webpack.config.js
@@ -4,51 +4,70 @@ const HtmlPlugin = require('html-webpack-plugin');
 const CssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
-module.exports = env => ({
-   mode: env,
-   entry: './src',
-   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[contenthash].js',
-      chunkFilename: '[name]-[contenthash].js'
-   },
-   resolve: {
-      extensions: ['.js', '.ts']
-   },
-   module: {
-      rules: [
-         {
-            test: /\.ts$/,
-            use: 'ts-loader'
-         },
-         {
-            test: /\.css$/,
-            use: [CssExtractPlugin.loader, 'css-loader']
-         },
-         {
-            test: /\.woff2?$/,
-            loader: 'file-loader',
-            options: {
-               name: 'fonts/[name]-[contenthash].[ext]'
+module.exports = env => {
+   const config = {
+      mode: env,
+      entry: './src',
+      output: {
+         path: path.resolve(__dirname, 'dist'),
+         filename: '[contenthash].js',
+         chunkFilename: '[name]-[contenthash].js'
+      },
+      resolve: {
+         extensions: ['.js', '.ts']
+      },
+      module: {
+         rules: [
+            {
+               test: /\.ts$/,
+               use: 'ts-loader'
+            },
+            {
+               test: /\.css$/,
+               use: [CssExtractPlugin.loader, 'css-loader']
+            },
+            {
+               test: /\.woff2?$/,
+               loader: 'file-loader',
+               options: {
+                  name: 'fonts/[name]-[contenthash].[ext]'
+               }
             }
-         }
-      ]
-   },
-   plugins: [
-      new CleanPlugin(),
-      new HtmlPlugin({
-         template: './index.html'
-      }),
-      new CssExtractPlugin({
-         filename: '[contenthash].css'
-      })
-   ],
-   optimization: {
-      minimizer: [
-         new TerserPlugin(),
-         new OptimizeCssPlugin()
-      ]
-   },
-   devtool: 'source-map'
-});
+         ]
+      },
+      plugins: [
+         new CleanPlugin(),
+         new HtmlPlugin({
+            template: './index.html'
+         }),
+         new CssExtractPlugin({
+            filename: '[contenthash].css'
+         })
+      ],
+      optimization: {
+         minimizer: [
+            new TerserPlugin(),
+            new OptimizeCssPlugin()
+         ]
+      },
+      devtool: 'source-map'
+   };
+
+   if (env === 'production') {
+      config.plugins.push(
+         new CompressionPlugin({
+            exclude: /\.map$/,
+            minRatio: 1
+         }),
+         new CompressionPlugin({
+            exclude: /\.map$/,
+            minRatio: 1,
+            algorithm: 'brotliCompress',
+            filename: '[path].br[query]'
+         })
+      );
+   }
+   return config;
+};
