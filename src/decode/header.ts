4f1bb1712f2052c27f878ff532421aa5ce48c4f0
matanui159 <josh@minteronline.com>
2020-04-20 12:25:19 +1000

    Fixed firefox support

--- src/decode/header.ts
+++ src/decode/header.ts
@@ -5,9 +5,9 @@ import { decodeWithUtif } from './utif';
 export async function decodeWithHeader(file: Blob): Promise<ImageData> {
    const headerBuffer = new Uint8Array(await readBlob(file.slice(0, 12)));
    const header = String.fromCharCode(...Array.from(headerBuffer));
-   if (/RIFF.{4}WEBP/s.test(header)) {
+   if (/^RIFF[\s\S]{4}WEBP/.test(header)) {
       return await decodeWithWebp(file);
-   } else if (/II\*\0|MM\0\*/s.test(header)) {
+   } else if (/^II\*\0|MM\0\*/.test(header)) {
       return await decodeWithUtif(file);
    } else {
       throw new Error(`Failed to detect header: ${headerBuffer}`);
