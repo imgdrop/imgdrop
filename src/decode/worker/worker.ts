6385e07bc813d4ce87987ec3e64946b9c5aec07f
matanui159 <josh@minteronline.com>
2020-04-29 22:25:43 +1000

    Disable native decoder

--- src/decode/worker/worker.ts
+++ src/decode/worker/worker.ts
@@ -9,7 +9,7 @@ export function callWorker<E extends keyof WorkerExports>(
    data: WorkerMessage<E>
 ): Promise<ReturnType<WorkerExports[E]>> {
    return new Promise((resolve, reject) => {
-      decodeWorker.onmessage = (event): void => reject(event.data);
+      decodeWorker.onmessage = (event): void => resolve(event.data);
       decodeWorker.onerror = (event): void => reject(new Error(event.message));
       decodeWorker.postMessage(data);
    });
